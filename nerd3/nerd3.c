e344e0e1e40d9b9e2269a4bff0f25c73689c6d1a
gnrr <hideaki.sh@gmail.com>
2013-05-30 11:56:15 +0900

    add: timer interrupt for hhk key sens

--- nerd3/nerd3.c
+++ nerd3/nerd3.c
@@ -5,14 +5,55 @@
  *  Device: ATMEGA88P
  */ 
 
-
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include "mystd.h"
 
+/* -------------------------------------------------
+ * definitions, globals and consts
+ */
+u1 cnt = 0;
+
+/* -------------------------------------------------
+ * interrupt service routines
+ */
+ISR(TIMER1_COMPA_vect)
+{
+    __di();
+
+    PORTB = 0xFE | (cnt++ % 2);         /* LED(PB0) invert */
+    
+    __ei();
+}
+/* -------------------------------------------------
+ * initializers
+ */
+static inline void init_devices(void)
+{
+    /* TIMER1 (16bit) */
+    TCCR1B = (0x01<<CS10) | (1<<WGM12); /* CLKio/1, CTC(Clear Timer on Compare Match) */
+                                        /* CLKio: 1 MHz = internal RC osc (factory default) */
+    TIMSK1 = 1<<OCIE1A;                 /* Compare A match interrupt */
+    OCR1A  = 200;                       /* row a => 200us cycled (T=400us) */
+    
+    /* PORTB: b7-b1:INPUT, b0:OUTPUT, all:HI */
+    DDRB  = 0x01;
+    PORTB = 0xFF;
+}
+
+/* -------------------------------------------------
+ *  main
+ */
 int main(void)
 {
+    init_devices();
+    __ei();
+
     while(1)
     {
         //TODO:: Please write your application code 
     }
+
+    return EXIT_SUCCESS;
 }
+/* this program ends here */
