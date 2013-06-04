4ffb78a51486511a1f9ef0e1e57c73b2d8f87fe0
gnrr <hideaki.sh@gmail.com>
2013-06-04 19:29:30 +0900

    ok: fixed chattering
    ng: not working key repeat

--- nerd3/nerd3.c
+++ nerd3/nerd3.c
@@ -16,9 +16,14 @@
 u1 HHKScanCode;
 u1 ScanBuf[64];                         // 64 keys (0:pressed)
 
-#define RINGBUF_SIZE 32
+#define RINGBUF_SIZE 128
 #include "ringbuf.h"
-RINGBUF TXBuf;
+RINGBUF TXBuf;//, TXBuf2;
+
+               /* 0     1     2     3     4     5     6     7     8     9     10   */
+               /* -     -     -    mod    -    scan1 scan2 scan3 scan4 scan5 scan6 */
+u1 REPORT[11] = {0xFD, 0x09, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
 
 /* -------------------------------------------------
  * initializers
@@ -45,7 +50,8 @@ static inline void init_devices(void)
     /* UART0: async, 9600, data=8, stop=1, pari=none */
     UBRR0  = 12;                                       // 1 MHz = internal RC osc (factory default)
     UCSR0A = (1<<U2X0);
-    UCSR0B = (1<<RXEN0) | (1<<TXEN0);                  /* enable receiver/transmitter */
+    UCSR0B = (1<<TXEN0);    /* enable int: RX Complete Interrupt Enable */
+                                                       /* enable pin: TXD/RXD */
 }
 
 static inline void init_rams(void)
@@ -59,12 +65,17 @@ static inline void init_rams(void)
 /* -------------------------------------------------
  * sub functions
  */
+#if 0
 static inline void uart_send(u1 ch)
 {
-  while( !(UCSR0A & (1<<UDRE0)) ) ;        /* wait for empty transmit buffer */
-
-  UDR0 = ch;
+    __di();
+ 
+    if(ringbuf_write(&TXBuf2, ch))
+        UCSR0B |=  (1<<UDRIE0);           /* enable data ready interrupt --> USART_UDRE_vect */
+ 
+    __ei();
 }
+#endif
 
 #if 0
 static inline void send_hid_release_all(void)
@@ -80,35 +91,49 @@ static inline void send_hid_release_all(void)
 
 static inline void send_hid_report(void)
 {
-                /* 0     1     2     3     4     5     6     7     8     9     10   */
-                /* -     -     -    mod    -    scan1 scan2 scan3 scan4 scan5 scan6 */
-    u1 rep[11] = {0xFD, 0x09, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    u1 *pmod  = &rep[3];
-    u1 *pscan = &rep[5];
+//              /* 0     1     2     3     4     5     6     7     8     9     10   */
+//              /* -     -     -    mod    -    scan1 scan2 scan3 scan4 scan5 scan6 */
+//  u1 rep[11] = {0xFD, 0x09, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    u1 *pmod, *pscan;
         
     if(TXBuf.unread_count == 0) return; // do nothing
 
+    REPORT[3] = 0x00;           // clear mod
+    for(u1 i=5; i<11; i++)
+        REPORT[i] = 0x00;       // clear scan1-6
+        
+    pmod  = &REPORT[3];
+    pscan = &REPORT[5];
+
     while(TXBuf.unread_count > 0) {
-        u1 c, m;
-        ringbuf_read(&TXBuf, &c);
-        m = modifier_bit(c);
+        u1 k, m;
+        ringbuf_read(&TXBuf, &k);
+        m = modifier_bit(k);
         if(m == 0x00) {
             // common keys
-            *pscan++ = HHK2HID_TBL[c];
-            if(pscan > &rep[10]) break;
+            *pscan++ = HHK2HID_TBL[k];
+            if(pscan > &REPORT[10]) break;
         } else {
             // modifier keys
             *pmod |= m;
         }
     }
 
-    for(u1 i=0; i<11; i++)
-        uart_send(rep[i]);              // send report 'pressed' something
-
-    if(TXBuf.unread_count == 0)         // prepare sending report 'release all' next call
+    if((TXBuf.unread_count == 0)         // prepare sending report 'release all' next call
+    && ((REPORT[3] != 0x00)
+     || (REPORT[5] != 0x00)))
         ringbuf_write(&TXBuf, 0x3F);    // 0x3F --> USID_NONE
+#if 0
+    for(u1 i=0; i<11; i++) {
+//        uart_send(rep[i]);              // send report 'pressed' something
+        ringbuf_write(&TXBuf2, rep[i]);
+    }
+#endif
+
+        UCSR0B |=  (1<<UDRIE0);           /* enable data ready interrupt --> USART_UDRE_vect */
 }
 
+#if 0
 static inline void test_send(u1 hhk_code)
 {
                 /* 0     1     2     3     4     5     6     7     8     9     10   */
@@ -131,6 +156,7 @@ static inline void test_send2(void)
     for(u1 i=0; i<11; i++)
         uart_send(rep[i]);
 }
+#endif
 
 /* -------------------------------------------------
  * main
@@ -150,19 +176,20 @@ int main(void)
 
     test_send2();    // adf
     test_send(0x3F); // none --> zifxadf
-#endif
 
     ringbuf_write(&TXBuf, 0x06); // x --> 0x1B
     ringbuf_write(&TXBuf, 0x23); // i --> 0x0C
     ringbuf_write(&TXBuf, 0x0E); // v --> 0x19
     send_hid_report();  // ivx
     send_hid_report();  // released all
+#endif
 
 
     while(1)
     {
         //TODO:: Please write your application code
-//        send_hid_report(); 
+        if((UCSR0B & (1<<UDRIE0)) == 0)           // Does REPORT[] free?
+            send_hid_report();
     }
 
     return EXIT_SUCCESS;
@@ -176,7 +203,7 @@ ISR(TIMER1_COMPA_vect)
     /* HHK key sense */
     u1 sw;
 
-    __di();
+//    __di();
 
     HHKScanCode = (HHKScanCode + 1) & 0x3F;
     PORTB = HHKScanCode | 0x40;         // #OE(PB6) --> HI
@@ -187,10 +214,25 @@ ISR(TIMER1_COMPA_vect)
     if(sw != ScanBuf[HHKScanCode]) {
         ScanBuf[HHKScanCode] = sw;      // update
         if(sw == 0) 
-            //uart_send(hhk2ascii_tbl[HHKScanCode]);
             ringbuf_write(&TXBuf, HHKScanCode);     // press --> send
     }
 
+//    __ei();
+}
+
+ISR(USART_UDRE_vect)
+{
+    __di();
+
+    static u1 *p = &REPORT[0];
+
+    if(p <= &REPORT[10]) {
+        UDR0 = *p++;                      // send a byte to uart
+    } else {
+        UCSR0B ^=  (1<<UDRIE0);         // disable data ready interrupt (make free REPORT[] )
+        p = &REPORT[0];
+    }
+    
     __ei();
 }
 
